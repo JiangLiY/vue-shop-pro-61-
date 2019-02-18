8b26aa257849498588ccefb44313e7d242775afb
JiangLiY <JiangLi0628@163.com>
2019-02-18 19:55:21 +0800

    用户权限列表开发完成

--- src/router/index.js
+++ src/router/index.js
@@ -5,35 +5,20 @@ import Login from '@/components/Login'
 import Home from '@/components/Home'
 import Welcome from '@/components/Welcome' // 具体是home的子组件
 import User from '@/components/User'
+
+// 导入权限列表的路由
+import Rights from '@/components/Rights' //具体是home的子组件
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    // {
-    //   path: '/',
-    //   name: 'HelloWorld',
-    //   component: HelloWorld
-    // }
-    {
-      path: '/',
-      redirect: '/login'
-    },
-    {
-      path: '/login',
-      component: Login
-    },
-    {
-      path: '/home',
-      component: Home,
-      redirect: '/welcome',
-      children: [{
-          path: '/welcome',
-          component: Welcome
-        },
-        {
-          path: '/users',
-          component: User
-        }
+    {path: '/', redirect: '/login'},
+    {path: '/login', component: Login},
+    {path: '/home', component: Home, redirect: '/welcome',children: [
+        {path: '/welcome', component: Welcome},
+        {path: '/users', component: User},
+        {path: '/rights', component: Rights}
       ]
     }
   ]
